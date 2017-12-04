442df4db70fb9ed03aeda2280c16f3f7ab5f694b
ksasidha <ksasidha@hotmail.com>
2017-12-04 14:41:10 -0600

    new changes

--- mmp3/src/main/java/com/project/mmp3/Library/Library.java
+++ mmp3/src/main/java/com/project/mmp3/Library/Library.java
@@ -19,7 +19,7 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Library {
-//changes to the libarray to kripa_branch    hgfyhhghh
+//changes to the libarray to kripa_branch    hgfyhhghhcvvbfbhghbgfh
 	
 	public static void captureScreenshot(WebDriver driver) throws IOException
 	{
